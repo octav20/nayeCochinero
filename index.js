90e7fb2e94b98bfe24818478955301d4d6eca7a9
octav20 <octav20>
2023-02-27 21:57:42 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+
+let cont = document.querySelector(".container")
+
+$(".button-82-pushable").click(function () {
+    if (cont.style.display === "none") {
+        cont.style.display = "block";
+    } else {
+        cont.style.display = "none";
+
+    }
+})
